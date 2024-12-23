ccc8a92e749f4ad918b2e25a272e7676fce1b398
taahabz <taahabz@gmail.com>
2024-12-23 18:07:25 +0500

    Update editor.py

--- src/editor.py
+++ src/editor.py
@@ -38,8 +38,6 @@ class Editor:
         self.settings = Settings()
         self.mouse_cam = MouseCam((0.25, 0.04), INITIAL_CAMERA_SCALE)
         # Camera update
-        # Camera update
-        # Camera update
 
     def update(self):
         # Camera update
